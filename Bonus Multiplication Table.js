36c5c58f4d54ec35395a068f29018018e706e8fd
bllnd <bllnd@mac.com>
2015-09-02 08:58:30 -0700

    Multiplication Table

---
+++ Bonus Multiplication Table.js
@@ -0,0 +1,7 @@
+for (var i =1 ; i <=10;i++){
+
+  for (var a=1 ; a<=10; a++){
+    var num = i;
+  console.log(i+"*"+a+"="num*a);
+}
+}
---
+++ Simple Multiplication Table.js
@@ -0,0 +1,4 @@
+for (var i =1 ; i <=10;i++){
+  var multiple = i*9;
+  console.log(i+"*9="+multiple);
+}
